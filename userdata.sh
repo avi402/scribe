a3a93cd045803a6245c0f63f065f5b96659a2a40
avi402 <alapati.avinash@gmail.com>
2021-11-22 13:56:38 +0530

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get -y update
+# Install OpenJDK 8
+sudo apt-get -y install openjdk-8-jdk
+# Install Jenkins
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
+sudo apt-get -y update
+sudo apt-get -y install jenkins
+sudo wget -qO - https://api.bintray.com/orgs/jfrog/keys/gpg/public.key | sudo apt-key add -
+echo "deb https://jfrog.bintray.com/artifactory-debs focal main" | sudo tee /etc/apt/sources.list.d/jfrog.list
+sudo apt update
+sudo apt install jfrog-artifactory-oss
+sudo systemctl start artifactory.service
+sudo systemctl enable artifactory.service
